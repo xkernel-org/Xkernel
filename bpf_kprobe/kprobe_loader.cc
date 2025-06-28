980e82c7bbdf10ebdd076126a1be28f7d527443b
zhongjiechen <chenzhongjie97@outlook.com>
2025-06-28 10:01:25 -0600

    Optimize kprobe_loader.cc

--- bpf_kprobe/kprobe_loader.cc
+++ bpf_kprobe/kprobe_loader.cc
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <dirent.h>
 #include <gflags/gflags.h>
 #include <sstream>
 #include <string>
@@ -14,11 +15,32 @@
 using namespace xkernel;
 
 DEFINE_string(files, "", "BPF files to load, separated by comma");
+DEFINE_bool(list, false, "List all available BPF files");
+DEFINE_bool(quiet, false, "Quiet mode, do not print any output");
 
 #define BPF_DIR "bpf/examples/"
 
 int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (FLAGS_list) {
+    // list all files in BPF_DIR
+    DIR *dir = opendir(BPF_DIR);
+    if (dir == NULL) {
+      fprintf(stderr, "Failed to open %s\n", BPF_DIR);
+      return 1;
+    }
+    printf("Available BPF files:\n");
+    struct dirent *entry;
+    int cnt = 0;
+    while ((entry = readdir(dir)) != NULL) {
+      if (strstr(entry->d_name, ".bpf.o") != NULL) {
+        printf("[%d] %s\n", cnt++, entry->d_name);
+      }
+    }
+    closedir(dir);
+    return 0;
+  }
 
   if (FLAGS_files.empty()) {
     fprintf(stderr, "files is not set\n");
@@ -45,8 +67,25 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  printf(
-      "Kprobe attached successfully. Check /sys/kernel/tracing/trace_pipe\n");
-  while (1)
-    sleep(1);
+  if (FLAGS_quiet) {
+    printf(
+        "Kprobe attached successfully. Check /sys/kernel/tracing/trace_pipe\n");
+    while (1) {
+      sleep(1);
+    }
+  } else {
+    // print the output of /sys/kernel/tracing/trace_pipe
+    FILE *fp = fopen("/sys/kernel/tracing/trace_pipe", "r");
+    if (fp == NULL) {
+      fprintf(stderr, "Failed to open /sys/kernel/tracing/trace_pipe\n");
+      return 1;
+    }
+    char line[1024];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+      printf("%s", line);
+    }
+    fclose(fp);
+  }
+
+  return 0;
 }
