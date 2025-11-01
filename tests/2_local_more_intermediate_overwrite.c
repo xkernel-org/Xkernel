f3db345974b225bc5c376694c72ac890aa76e62c
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-01 17:18:04 -0500

    tests: ends MACRO effect by overwriting the variable

---
+++ tests/2_local_more_intermediate_overwrite.c
@@ -0,0 +1,22 @@
+#define MACRO 3600
+
+int foo() {
+    int a = 100;
+    int b = MACRO;
+    int c, d, e, f, g;
+    int z;
+
+    z = b;
+
+    b = 42;
+
+    c = b + a;
+    d = c - b;
+    e = d * a;
+    f = e / b;
+    g = f % c;
+
+    if (a == g)
+        return 1;
+    return 0;
+}
