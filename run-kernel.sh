f07c983e3c5b0557ab65cdcac5777c7c28a53052
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-02 19:31:56 -0600

    some examples in kernel

--- run-kernel.sh
+++ run-kernel.sh
@@ -2,22 +2,68 @@
 
 set -ex
 
-export PATH=/usr/lib/llvm-20/bin:$PATH
+if ! command -v opt >/dev/null 2>&1; then
+    export PATH=/usr/lib/llvm-20/bin:$PATH
+    if ! command -v opt >/dev/null 2>&1; then
+        echo "opt not found"
+        exit 1
+    fi
+fi
 
-# We use -disable-output and -o /dev/null because we only care about the
-# printouts (errs()) from our pass, not a modified .bc file.
-#
-# Usage: taint-tracker<function_name;opcode;constant_value;debug>
-# - function_name: name of the function to analyze
-# - opcode: target instruction opcode (e.g., "store", "call", "add", etc. or empty for all)
-# - constant_value: the constant integer value to track
-# - debug: verbose debug mode (true/false/1/0, default: false)
-#
-# Example with parameters:
-# opt -load-pass-plugin=build/libTaintTrackerPass.so -passes="taint-tracker<gss_fill_context;store;3600;true>" ...
-#
-# Using defaults (gss_fill_context, any opcode, 3600, debug off):
+### 1. DFR_MAX (1)
+
+KERNEL_DIR=/media/wd-sn580-2t-1/users/wentaoz5/xkernel/defuse/linux-wllvm
+SOURCE_FILE=net/sunrpc/cache.c
+FUNCTION_NAME=cache_check_rcu
+SOURCE_OP=icmp
+CONSTANT_VALUE=301
+
+### 1. DFR_MAX (2)
+
+KERNEL_DIR=/media/wd-sn580-2t-1/users/wentaoz5/xkernel/defuse/linux-wllvm
+SOURCE_FILE=net/sunrpc/cache.c
+FUNCTION_NAME=cache_check_rcu
+SOURCE_OP=icmp
+CONSTANT_VALUE=300
+
+### 2. GSSD_MIN_TIMEOUT
+
+KERNEL_DIR=/media/wd-sn580-2t-1/users/wentaoz5/xkernel/defuse/linux-wllvm
+SOURCE_FILE=net/sunrpc/auth_gss/auth_gss.c
+FUNCTION_NAME=gss_fill_context
+SOURCE_OP=select
+CONSTANT_VALUE=3600
+
+# TODO Not compiled in defconfig
+
+### 3. SMC_TX_WORK_DELAY
+
+### 4. SMC_LGR_NUM_INCR
+
+### 5. RDS_IB_RECYCLE_BATCH_COUNT
+
+### 6. QUEUE_THRESHOLD
+
+### 7. PIE_SCALE
+
+### 8. BUSY_POLL_BUDGET
+
+OBJ_FILE=$(dirname $SOURCE_FILE)/$(basename $SOURCE_FILE .c).o
+BC_FILE=$(dirname $SOURCE_FILE)/$(basename $SOURCE_FILE .c).bc
+LL_FILE=$(dirname $SOURCE_FILE)/$(basename $SOURCE_FILE .c).ll
+VMLINUX_BC_FILE=$KERNEL_DIR/vmlinux.bc
+
+(
+    cd $KERNEL_DIR
+    extract-bc $OBJ_FILE -o $BC_FILE
+    llvm-dis $BC_FILE -o $LL_FILE
+)
+
 opt -load-pass-plugin=build/libTaintTrackerPass.so \
-    -passes="taint-tracker<gss_fill_context;select;3600;true>" \
+    -passes="taint-tracker<$FUNCTION_NAME;$SOURCE_OP;$CONSTANT_VALUE;false>" \
     -disable-output \
-    /media/wd-sn580-2t-1/users/wentaoz5/xkernel/defuse/linux-wllvm/net/sunrpc/auth_gss/auth_gss.bc
+    $KERNEL_DIR/$BC_FILE \
+    `#$VMLINUX_BC_FILE`
+
+echo "$KERNEL_DIR/$LL_FILE"
+echo "$KERNEL_DIR/$SOURCE_FILE"
