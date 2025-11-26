7bc0ddcaf99ebbc80a938e04e3d17f52f55bd06d
Yulong Tang <yulongtang077@gmail.com>
2025-11-26 07:58:40 -0500

    Add trigger counts per second

--- Experiment/mm/shrink_batch/zswap_shrinker/scripts/zswap_min.sh
+++ Experiment/mm/shrink_batch/zswap_shrinker/scripts/zswap_min.sh
@@ -7,9 +7,10 @@ sudo systemd-run --unit=$UNIT \
     --same-dir --collect --pty \
     numactl --cpunodebind=0 --membind=0 \
     time ./bin/zswap_min --total-mb 4096 --block-pages 128 --reuse-dist 16 \
-               --warmup-passes 3 --burst 12 --loops 2000 --file ./res/16.txt
+               --warmup-passes 3 --burst 12 --loops 2000 \
+               --file ./res/20251126-034820/8.txt
 
-sudo chown yltang:xkernel-PG0 ./res/16.txt
+sudo chown yltang:xkernel-PG0 ./res/20251126-034820/8.txt
 
 # To check the status of this serviee:
 # sudo systemctl status $UNIT
