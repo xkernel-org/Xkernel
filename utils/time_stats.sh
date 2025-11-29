ff58bc143e51c51ff52d6f6f0e74b26b51b489fd
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-29 16:47:31 -0600

    utils: mem stats

--- utils/time_stats.sh
+++ utils/time_stats.sh
@@ -3,3 +3,11 @@
 for f in kernel-results/*/*.time.txt; do
     grep 'Elapsed (wall clock) time' $f
 done | awk '{print $8}' | python utils/time_stats.py
+
+TMPFILE=$(mktemp)
+for f in kernel-results/*/*.time.txt; do
+    grep 'Maximum resident set size (kbytes)' $f
+done | awk '{print $6}' | sort -n > $TMPFILE
+echo "Max memory: $(tail -n 1 $TMPFILE) kB"
+echo "Min memory: $(head -n 1 $TMPFILE) kB"
+rm -f $TMPFILE
