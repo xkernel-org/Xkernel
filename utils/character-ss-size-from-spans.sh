a6c8da2eb89e69e7e1bc8bce35518e5e97dadc2d
Wentao Zhang <zhangwt1997@gmail.com>
2025-12-08 23:19:05 -0600

    utils: sorry for this number hell

--- utils/character-ss-size-from-spans.sh
+++ utils/character-ss-size-from-spans.sh
@@ -17,8 +17,9 @@ python utils/character-ss-size-from-spans.py vmlinux-func-bb-sizes.txt
 
 NUM_SS_SIZE_FILES=$(ls kernel-results/*/ss-size2.txt | wc -l)
 
-if [[ $NUM_SS_SIZE_FILES -ne 136 ]]; then
-    echo "Error: Expected 136 ss-size2.txt files, but found $NUM_SS_SIZE_FILES"
+# 140 - 1 duplicate - 4 huge IR diff + 1 partly analyzed despite huge IR diff + 1 KLP bad case
+if [[ $NUM_SS_SIZE_FILES -ne 137 ]]; then
+    echo "Error: Expected 137 ss-size2.txt files, but found $NUM_SS_SIZE_FILES"
     exit 1
 fi
 
@@ -36,9 +37,10 @@ for f in kernel-results/*/ss-size2.txt; do
     grep -E "Total: [0-9]+ instructions" $f
 done | awk '{ print $2 }' | tee kernel-results/per-perf-const-size.txt
 
+# 140 - 1 duplicate - 4 huge IR diff
 NUM_SIZES=$(cat kernel-results/per-perf-const-size.txt | wc -l)
-if [[ $NUM_SIZES -ne 134 ]]; then
-    echo "Error: Expected 134 sizes, but found $NUM_SIZES"
+if [[ $NUM_SIZES -ne 135 ]]; then
+    echo "Error: Expected 135 sizes, but found $NUM_SIZES"
     exit 1
 fi
 
