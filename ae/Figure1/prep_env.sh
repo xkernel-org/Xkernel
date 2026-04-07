685423a8203daf95a03745cc0e58e41234df51dc
zhongjiechen <chenzhongjie97@outlook.com>
2026-04-07 00:51:49 -0500

    Nits.

--- ae/Figure1/prep_env.sh
+++ ae/Figure1/prep_env.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y fio liburing-dev
 
-python gen_iolog.py --dev nvme1n1 --op write --outfile iolog_write.txt
+python gen_iolog.py --dev sdb --op write --outfile iolog_write.txt
